030716aeee56a57ce77fd2e6b608a980c3b82c7e
Prelang Builder <builder@prelang.com>
2014-04-09 07:57:51 +0000

    Initial commit; clean Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Avy::Application.config.secret_key_base = '9aa3b6e6a5c331048cee104af2af101456aee0ceda9942c58e8ceeb7662ae51a5a7118647035284b2ea863c2e1fab6456a8468650175fbd162afbab5d814e75b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Avy::Application.config.session_store :cookie_store, key: '_avy_session'
